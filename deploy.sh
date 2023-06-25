3e1844755391c6195ca2c07bca437063c45545d3
Jellinek <jellinekbraus@gmail.com>
2023-06-25 22:37:52 +0500

    deploy-fix

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,11 @@
 set -e
 npm run build
 
-cd dist
+cd build
 
 git init
 git add -A
 git commit -m 'deploy'
-
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 git push -f https://github.com/iibratik/mywish.git main:gh-pages
 
